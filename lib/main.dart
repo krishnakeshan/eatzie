df14721700ce82eff6ef20e33dfe8a7d8ebc5f06
Krishna Keshan <krishnakeshan18@gmail.com>
2019-01-06 23:31:48 +0530

    added NearbyLocationListViewItem and a bunch of other stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,274 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(EatzieApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class EatzieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Eatzie',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  //properties
+  int _selectedIndex = 0;
+  final _tabTitles = ["Home", "Feed", "Inbox", "Orders", "Profile"];
+  final _children = [
+    HomeWidget(),
+    HomeWidget(),
+    HomeWidget(),
+    HomeWidget(),
+    HomeWidget(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(_tabTitles[_selectedIndex]),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Colors.deepOrange,
+            tooltip: "Cart(s)",
+            onPressed: () {},
+          )
+        ],
+        centerTitle: false,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+            backgroundColor: Colors.deepOrange,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.layers),
+            title: Text("Feed"),
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_1),
+            title: Text("Orders"),
+            backgroundColor: Colors.deepOrange,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble),
+            title: Text("Inbox"),
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.face),
+            title: Text("Profile"),
+            backgroundColor: Colors.black,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (int selected) {
+          print("something");
+          setState(() {
+            _selectedIndex = selected;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext buildContext) {
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: Text(
+                  "Near You",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 17, 15, 16),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 200.0,
+                padding: EdgeInsets.fromLTRB(0, 10, 10, 0),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    NearbyLocationListViewItem(),
+                    NearbyLocationListViewItem(),
+                    NearbyLocationListViewItem(),
+                    NearbyLocationListViewItem(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class NearbyLocationListViewItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext buildContext) {
+    return Card(
+      child: Container(
+        width: 150,
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Stack(
+              children: <Widget>[
+                Positioned(
+                  child: Image.network(
+                    "https://www.theriverside.co.uk/images/Inside-Restaurant.jpg",
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 2,
+                  child: IconButton(
+                    icon: Icon(Icons.bookmark),
+                    color: Colors.white,
+                    tooltip: "Bookmark",
+                    onPressed: () {
+                      print("bookmarked");
+                    },
+                  ),
+                ),
+                Positioned(
+                  bottom: 8,
+                  left: 8,
+                  child: Card(
+                    child: Container(
+                      padding: EdgeInsets.all(4.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.star,
+                            size: 10,
+                            color: Colors.red,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 1.0, right: 1.0),
+                            child: Text(
+                              "2.4",
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 8,
+                  bottom: 8,
+                  child: Card(
+                    child: Container(
+                      padding: EdgeInsets.all(4.0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.link,
+                            color: Colors.red,
+                            size: 10,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 2),
+                            child: Text(
+                              "4",
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Container(
+              padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 4.0),
+              child: Container(
+                child: Text(
+                  "Madouk Cafe",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 17, 15, 16),
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment(0, 0),
+              padding: EdgeInsets.fromLTRB(8, 2, 8, 2),
+              child: Text(
+                "9429 friends have been here",
+                maxLines: 2,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment(0, 0),
+                child: Text(
+                  "0.5 kms away",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      elevation: 4.0,
+      clipBehavior: Clip.antiAlias,
     );
   }
 }
