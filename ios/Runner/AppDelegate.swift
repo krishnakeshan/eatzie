3fbee450a56b0889414ca842a6a12047f7f3fb58
Krishna Keshan <krishnakeshan18@gmail.com>
2019-02-03 21:09:23 +0530

    added log in logic

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,128 @@
 import UIKit
 import Flutter
+import AccountKit
+import Firebase
+import FirebaseAuth
+import Parse
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    //MARK: Properties
+    var flutterResults: [String : FlutterResult] = [:]
+    var accountKit: AKFAccountKit! = nil
+    
+    //MARK: Methods
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+        ) -> Bool {
+        
+        //Configure Firebase
+        FirebaseApp.configure()
+        
+        //Initialize Parse
+        let parseClientConfiguration = ParseClientConfiguration {
+            $0.applicationId = "87c27ca0-2133-400c-aadf-beb43d430315"
+            $0.server = "http://34.73.142.234:1337/eatzie"
+        }
+        
+        Parse.initialize(with: parseClientConfiguration)
+        
+        //get a view controller
+        let viewController: FlutterViewController = window.rootViewController! as! FlutterViewController
+        
+        //declare channels
+        let mainChannel = FlutterMethodChannel(name: "com.qrilt.eatzie/main", binaryMessenger: viewController)
+        
+        //set method call handlers
+        //set method call handler for main channel
+        mainChannel.setMethodCallHandler { (flutterMethodCall, flutterResult) in
+            //determine which method called and perform actions accordingly
+            if flutterMethodCall.method == "initiateLogin" {
+                //set result object
+                self.flutterResults["accountKit"] = flutterResult
+                
+                //start Account Kit login flow
+                self.initiateLogin(rootViewController: viewController)
+            }
+        }
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    //method to start FB Account Kit login
+    func initiateLogin(rootViewController: FlutterViewController) {
+        accountKit = AKFAccountKit(responseType: .accessToken)
+        
+        //check if existing account kit token is there
+        if accountKit.currentAccessToken != nil {
+            //there's an existing account kit token, use it further
+            logInUser(accessToken: accountKit.currentAccessToken!)
+        } else {
+            //there's no existing account kit token, show login screen to get one
+            let inputState = UUID().uuidString
+            let loginViewController: (UIViewController & AKFViewController)! = accountKit.viewControllerForPhoneLogin(with: nil, state: inputState)
+            
+            //prepare loginViewController
+            loginViewController.delegate = self
+            loginViewController.enableGetACall = true
+            loginViewController.enableSendToFacebook = true
+            
+            //proceed to show if loginViewController is not nil
+            if loginViewController != nil {
+                rootViewController.present(loginViewController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    //method to log in user on server side
+    func logInUser(accessToken: AKFAccessToken) {
+        //get account kit parameters, to pass to cloud function
+        accountKit.requestAccount { (accountKitAccount, error) in
+            if error == nil && accountKitAccount != nil {
+                print("successfully verified number, calling cloud function")
+                //prepare params to call cloud function
+                let params = [
+                    "accountKitId": accountKitAccount!.accountID,
+                    "accessToken": accessToken.tokenString,
+                    "lastRefresh": accessToken.lastRefresh.description,
+                    "phoneCountryCode": accountKitAccount!.phoneNumber!.countryCode,
+                    "phoneNumber": accountKitAccount!.phoneNumber!.phoneNumber
+                ]
+                
+                //call cloud function
+                PFCloud.callFunction(inBackground: "logInUser", withParameters: params, block: { (result, error) in
+                    if error == nil {
+                        //cloud function returns tokens to log in to Parse and Firebase
+                        //get tokens
+                        let parseSessionToken = String((result as! String).split(separator: "|")[0])
+                        let firebaseSessionToken = String((result as! String).split(separator: "|")[1])
+
+                        //log into Parse
+                        PFUser.become(inBackground: parseSessionToken, block: { (parseUser, error) in
+                            if error == nil {
+                                //logged into Parse, now log into Firebase
+                                Auth.auth().signIn(withCustomToken: firebaseSessionToken) { (user, error) in
+                                    if error == nil {
+                                        //signed in with Firebase, return to Flutter
+                                        let accountKitResult = self.flutterResults["accountKit"]!
+                                        accountKitResult(true)
+                                    }
+                                }
+                            }
+                        })
+                        
+                    }
+                })
+            }
+        }
+    }
+}
+
+//Account Kit View Controller Delegate Methods
+extension AppDelegate: AKFViewControllerDelegate {
+    //method to handle successful login
+    func viewController(_ viewController: (UIViewController & AKFViewController)!, didCompleteLoginWith accessToken: AKFAccessToken!, state: String!) {
+        logInUser(accessToken: accessToken)
+    }
 }
--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,12 +1,92 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class WelcomeScreenWidget extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class WelcomeScreenWidget extends StatefulWidget {
+  @override
+  _WelcomeScreenWidgetState createState() {
+    return _WelcomeScreenWidgetState();
+  }
+}
+
+class _WelcomeScreenWidgetState extends State<WelcomeScreenWidget> {
+  //Properties
+  static const platform = const MethodChannel("com.qrilt.eatzie/main");
+  bool isLoggedIn = false;
+
+  //Methods
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[],
+      backgroundColor: Colors.deepOrange,
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "Welcome To Eatzie",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 24),
+                child: RaisedButton(
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        "Get Started",
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 8),
+                        child: Icon(
+                          Icons.arrow_forward,
+                          size: 16,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    //call method to initiate login
+                    _initiateLogin();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  //method to initiate login
+  Future<void> _initiateLogin() async {
+    try {
+      print("going native");
+      bool loginResult = await platform.invokeMethod("initiateLogin");
+      print("returned from native $loginResult");
+
+      //set state accordingly
+      setState(() {
+        isLoggedIn = loginResult;
+      });
+    } on PlatformException catch (e) {
+      print("error initiating login: ${e.message}");
+    }
   }
 }
